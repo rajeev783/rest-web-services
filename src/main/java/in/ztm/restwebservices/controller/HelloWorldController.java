18c016ac6c492b728b4136316edb318e84e024e1
Rajeev Kumar <rjvkumar783@gmail.com>
2021-11-01 07:52:44 +0530

    internationalization

--- src/main/java/in/ztm/restwebservices/controller/HelloWorldController.java
+++ src/main/java/in/ztm/restwebservices/controller/HelloWorldController.java
@@ -1,14 +1,38 @@
 package in.ztm.restwebservices.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 	
+	@Autowired
+	private MessageSource messageSource;
+	
 	@GetMapping(path="/hello-world")
 	public String helloWorld(){
 		return "Hello World";
+	}
+	
+	/*@GetMapping(path="/hello-world-i18n")
+	public String helloWorldI18n(
+			@RequestHeader(name="Accept-Language", required=false) Locale locale
+			){
+		System.out.println(locale);
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+	}*/
+	
+	@GetMapping(path="/hello-world-i18n")
+	public String helloWorldI18n(
+			//@RequestHeader(name="Accept-Language", required=false) Locale locale
+			){
+		return messageSource.getMessage("good.morning.message", null, "Default Message", LocaleContextHolder.getLocale());
 	}
 
 }
